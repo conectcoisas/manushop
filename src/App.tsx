a00ef557ba539ab3d839e7578f147565bb726bc0
Danilo Gomes <danilaum.gomes@hotmail.com>
2023-11-02 13:49:56 -0300

    programando painel principal

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,32 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import Column from './components/Column';
 import { ColumnType } from './utils/enums';
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  var with01 = "65%"
+  var with02 = '20%'
+  var with03 = '5%'
+
+  const columnWhithPedidos: string[] = [with01, with02, with03, with03, with03]
+  const columnWhithCorte: string[] = [with02, with01, with02, with03, with03]
+  const columnWhithSilk: string[] = [with03, with02, with01, with02, with03]
+  const columnWhithCostura: string[] = [with03, with03, with02, with01, with02]
+  const columnWhithRevisao: string[] = [with03, with03, with03, with02, with01]
+
+  const columnsWhith = [ 
+    columnWhithPedidos, 
+    columnWhithCorte, 
+    columnWhithSilk, 
+    columnWhithCostura, 
+    columnWhithRevisao ]
+ 
+  const [rezize, setResize] = useState(0)
+  const btnSetRezize = (rezizeColumn) => { setResize(rezizeColumn); 
+    console.log(rezizeColumn)
+   }
+
   return (
     <main>             
       <div className="topo">    
@@ -14,11 +38,11 @@ function App() {
       </div>  
       <div className="container">
         <DndProvider backend={HTML5Backend}>  
-          <Column column={ColumnType.PEDIDO}  /> 
-          <Column column={ColumnType.CORTE} />
-          <Column column={ColumnType.SILK} /> 
-          <Column column={ColumnType.COSTURA} />    
-          <Column column={ColumnType.REVISAO} />                     
+          <Column width={columnsWhith[rezize][0]} column={ColumnType.PEDIDO} onRezize={btnSetRezize} /> 
+          <Column width={columnsWhith[rezize][1]} column={ColumnType.CORTE} onRezize={btnSetRezize} />
+          <Column width={columnsWhith[rezize][2]} column={ColumnType.SILK} onRezize={btnSetRezize}/> 
+          <Column width={columnsWhith[rezize][3]} column={ColumnType.COSTURA} onRezize={btnSetRezize}/>    
+          <Column width={columnsWhith[rezize][4]} column={ColumnType.REVISAO} onRezize={btnSetRezize}/>                     
         </DndProvider>         
       </div>      
     </main>
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,26 +1,33 @@
-import {  Box,  Button,  Flex,  IconButton,  Stack, color  } from '@chakra-ui/react';
+import {  Box,  Button, Collapse   } from '@chakra-ui/react';
 import useColumnDrop from '../hooks/useColumnDrop';
 import useColumnTasks from '../hooks/useColumnTasks';
 import { ColumnType } from '../utils/enums';
 import Task from './Task';
+import { useState } from 'react'
 
-
-function Column({ column }: { column: ColumnType }) {
+function Column({ width, column, onRezize }: { width: string, column: ColumnType, onRezize: Function}) {
 
   const { tasks, addEmptyTask, deleteTask, dropTaskFrom, swapTasks, updateTask } = useColumnTasks(column);
   const { dropRef, isOver } = useColumnDrop(column, dropTaskFrom);  
  
   
   const ColumnTasks = 
-  tasks.map((task, index) => (<Task key={task.id} task={task} index={index}  onDropHover={swapTasks}  
-    onUpdate={updateTask} 
-    onDelete={deleteTask} /> 
-  ));
-  
+  tasks.map((task, index) => (  <Task key={task.id} task={task} index={index}  
+    onDropHover={swapTasks}  onUpdate={updateTask}  onDelete={deleteTask} />  ));  
+
+
+  var retRezize = {pedidos: 0, corte: 1, silk: 2, costura: 3, revisao: 4 }
+  var retRezizeNumber = retRezize[column]
+
   return (     
-    <Box w='20%' display='flex' m='1'>  
+    <Box display='flex' m='1'  w={width}>  
         <Box ref={dropRef} w='100%'>           
-            <Button onClick={ () => {addEmptyTask()} }>{column}</Button>
+            <Button onClick={ 
+              () => { 
+                addEmptyTask()
+                onRezize(retRezizeNumber)
+                }}
+            >{column}</Button>
             {ColumnTasks}             
         </Box>
     </Box>  
